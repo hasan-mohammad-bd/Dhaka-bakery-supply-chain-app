e7852cf38b400a35dc866aba15f227e808a61a2d
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-06-09 08:32:45 +0600

    remove react query

--- src/hook/useProducts.js
+++ src/hook/useProducts.js
@@ -4,13 +4,12 @@ import { useEffect, useState } from "react";
 
 const useProducts = () =>{
     const [products, setProducts] = useState([]);
-
+    console.log(products);
+  
     useEffect(() => {
-      if(products){
-        fetch("https://stark-dusk-94643.herokuapp.com/product/")
+      fetch("https://stark-dusk-94643.herokuapp.com/product/")
         .then((res) => res.json())
         .then((data) => setProducts(data));
-      }
     }, [products]);
 
     return [products, setProducts]
